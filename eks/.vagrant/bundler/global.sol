8341abe6d82424459809d4bcbb89361f463b1a99
Samuel Nnanna <samuelnnanna71@gmail.com>
2024-03-03 00:52:42 +0100

    updates on kube branch

--- eks/.vagrant/bundler/global.sol
+++ eks/.vagrant/bundler/global.sol
@@ -1,1 +1,1 @@
-{"dependencies":[["vagrant-hostmanager",["= 1.8.9"]]],"checksum":"ddf331fa318025f20903ea05638e26434cc3cf7e617d374e3d1391d35b01f211","vagrant_version":"2.2.9"}
+{"dependencies":[["vagrant-vmware-desktop",["= 3.0.3"]]],"checksum":"838409c1111983ab2a8df5e2303bbbf1b5f75f442c7fbc7dc95d71624d837f20","vagrant_version":"2.4.0"}
